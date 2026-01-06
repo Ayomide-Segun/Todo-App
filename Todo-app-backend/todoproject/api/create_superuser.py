e660e529787acb1483d4cbf734321efbeafa8386
Ayomide-Segun <treasure.segun@gmail.com>
2026-01-06 21:28:32 +0100

    Force reset superuser password

--- Todo-app-backend/todoproject/api/create_superuser.py
+++ Todo-app-backend/todoproject/api/create_superuser.py
@@ -1,20 +1,24 @@
 import os
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
 
 def run():
-    username = os.getenv("DJANGO_SUPERUSER_USERNAME")
-    password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
     if not username or not password:
+        print("superuser env var missing")
         return
 
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            password=password
-        )
-        print("Superuser created")
-    else:
-        print("Superuser already exists")
+    user, created = User.objects.get_or_create(
+        username=username
+    )
+
+    # 🔥 FORCE password reset every deploy
+    user.set_password(password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.save()
+
+    print("Superuser ensured:", username)
