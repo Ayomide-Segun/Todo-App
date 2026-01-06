a5db3b176449e0a8e3a71538d848cfc823c34ff8
Ayomide-Segun <treasure.segun@gmail.com>
2026-01-06 19:07:55 +0100

    Auo-create superuser on render

---
+++ Todo-app-backend/todoproject/api/create_superuser.py
@@ -0,0 +1,20 @@
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def run():
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+    if not username or not password:
+        return
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            password=password
+        )
+        print("Superuser created")
+    else:
+        print("Superuser already exists")
