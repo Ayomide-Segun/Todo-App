762a37ff9138cffba19aa8e3cff5e523064241d8
Ayomide-Segun <treasure.segun@gmail.com>
2026-01-12 20:14:34 +0100

    solve 'auth_table not found' issue

--- Todo-app-backend/todoproject/api/management/commands/createsu.py
+++ Todo-app-backend/todoproject/api/management/commands/createsu.py
@@ -1,11 +1,16 @@
 import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.db import connection
 
 class Command(BaseCommand):
     help = "Create or update a superuser from env vars"
 
     def handle(self, *args, **kwargs):
+        if "auth_user" not in connection.introspection.table_names():
+            print("Auth table not ready, skipping superuser creation")
+            return
+        
         User = get_user_model()
 
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
