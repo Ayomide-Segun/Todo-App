4f96c24c5a0a55825e1461cdb0f67e88b62b3a74
Ayomide-Segun <treasure.segun@gmail.com>
2026-01-15 19:43:16 +0100

    fixed email verification logic

--- Todo-app-frontend/todoapp/src/contexts/AuthContext.jsx
+++ Todo-app-frontend/todoapp/src/contexts/AuthContext.jsx
@@ -45,6 +45,7 @@ export function AuthContextProvider({children}){
         navigate('/verifyEmail')
         } catch (err) {
             console.log(err)
+            alert('Network error, try again later')
         }
     }
 
