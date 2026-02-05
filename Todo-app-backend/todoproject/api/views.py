b96ea3c53b28cd8928cbfa20defd1a08cb47b3af
Ayomide-Segun <treasure.segun@gmail.com>
2026-02-05 20:58:54 +0100

    fixed e mail logic

--- Todo-app-backend/todoproject/api/views.py
+++ Todo-app-backend/todoproject/api/views.py
@@ -124,7 +124,7 @@ def verifyEmail(request):
     verification_link = f"http://task-management-app-virid.vercel.app/verifyEmail"
     try:
         resend.Emails.send({
-            "from": "Todo App <treasure.segun@gmail.com>",
+            "from": "Todo App <onboarding@resend.dev>",
             "to": [email],
             "subject": "Email verification",
             "html": f"""
